43f100dd0835b31c96dbd77809bf97fd12fed3a8
Dakyeong Lee <dlekrud0503@gmail.com>
2016-06-07 20:52:05 +0900

    #3 Change Dialog

--- app/src/main/java/kr/hs/emirim/good005003/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/good005003/simpledialog/MainActivity.java
@@ -1,13 +1,43 @@
 package kr.hs.emirim.good005003.simpledialog;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    String[] items = {"젤리빈", "킷켓", "롤리팝"};
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        but = (Button)findViewById(R.id.but_dialog);
+        but.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("First Dialog");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                but.setText(items[which]);
+            }
+        });
+        dialog.setMessage("This is message part.");
+        dialog.setIcon(R.drawable.Mushroom1UP);
+        dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "대화상자의 확인 버튼을 클릭했음.", Toast.LENGTH_LONG).show();
+            }
+        });
+        dialog.show();
     }
 }
